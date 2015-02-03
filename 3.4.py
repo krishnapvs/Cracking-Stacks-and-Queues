e764c5c679228f1f115f6df423edd7ba7b7e6d1e
Krishna VS Puvvada <vpuvvad@ncsu.edu>
2015-02-03 00:00:43 -0500

    Update 3.4.py

--- 3.4.py
+++ 3.4.py
@@ -10,5 +10,22 @@ class Stack():
 	def pop(self,index):
 		return self.towers[index].pop()
 
-def moveDisks(n,origin,destination,temp):
-	
+	def moveDisks(self,n,origin,destination,temp):
+		# base case 
+		if(n <= 0):
+			return
+		self.moveDisks(n-1,origin,temp,destination)
+		dummy=self.towers[origin].pop()
+		self.towers[destination].append(dummy)
+		self.moveDisks(n-1, temp,destination,origin)
+
+S=Stack()
+
+for i in range(10,0,-1):
+	S.push(0,i)
+
+print S.towers
+
+S.moveDisks(10,0,2,1)
+
+print S.towers
