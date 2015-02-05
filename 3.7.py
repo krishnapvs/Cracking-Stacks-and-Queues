867d8d6accd6dcbdc27329f356198e1f8076a7e3
krishnapvs <vpuvvad@ncsu.edu>
2015-02-05 17:00:44 -0500

    3.7 v1

---
+++ 3.7.py
@@ -0,0 +1,25 @@
+''' An animal shelter holds only dogs and cats, and operates on a strictly "first in,
+first out" basis. People must adopt either the "oldest" (based on arrival time) of
+all animals at the shelter, or they can select whether they would prefer a dog or
+a cat (and will receive the oldest animal of that type). They cannot select which
+specificanimal they would like. Create the data structures to maintain this system
+and implement operations such as enqueue, dequeueAny, dequeueDog and
+dequeueCat.You may use the built-in LinkedList data structure'''
+
+class Shelter:
+	def __init__(self,name):
+		self.name=name
+
+	class Node:
+		def __init__(self,cargo=None,kind=None,next=None):
+			self.name=cargo
+			self.kind=kind
+			self.next=next
+
+	def enqueue(self,type,name):
+		head=self
+		while(head.next):
+			head=head.next
+		head.next
+
+
